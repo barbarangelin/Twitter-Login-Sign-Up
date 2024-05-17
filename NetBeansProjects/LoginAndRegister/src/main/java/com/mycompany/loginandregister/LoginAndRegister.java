8188e5d566201f793fccafdf39048079267c3e42
Barbara Angelin <barbarangelin@gmail.com>
2024-05-16 21:03:07 -0300

    Login & Sign Up

---
+++ NetBeansProjects/LoginAndRegister/src/main/java/com/mycompany/loginandregister/LoginAndRegister.java
@@ -0,0 +1,17 @@
+
+
+package com.mycompany.loginandregister;
+
+/**
+ *
+ * @author barbara
+ */
+public class LoginAndRegister {
+
+    public static void main(String[] args) {
+      LoginWindow loginframe =  new LoginWindow();
+      loginframe.setVisible(true);
+      loginframe.pack();
+      loginframe.setLocationRelativeTo(null);
+    }
+}
